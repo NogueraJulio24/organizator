f0b7a33b3b1570853834adab2b283f20fbc01d5f
Julio Noguera <noguerajulio24@gmail.com>
2020-11-01 23:05:12 -0500

    update

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,7 @@ class ApplicationController < ActionController::Base
   before_action :authenticate_user!
 
   rescue_from CanCan::AccessDenied do |exception|
-    redirect_to root_path
+    redirect_to authenticated_root_path
   end
 
   def set_locale
