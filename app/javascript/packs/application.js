5b7f052c6071ec2c8cf2de3b5def5375ed6c07d2
Julio Noguera <noguerajulio24@gmail.com>
2020-11-01 22:40:54 -0500

    update i18

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,8 +8,11 @@ require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
 
-import 'cocoon'
+import "bootstrap"
 
+import "roboto-fontface"
+
+import "cocoon"
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
@@ -17,3 +20,5 @@ import 'cocoon'
 //
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
+
+import "../src/stylesheets/application"
