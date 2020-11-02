6dc491d29c71d6084ed74564ba8438f0e814b765
Julio Noguera <=>
2020-11-01 20:04:25 -0500

    adding test

---
+++ spec/factories/categories.rb
@@ -0,0 +1,16 @@
+# == Schema Information
+#
+# Table name: categories
+#
+#  id          :bigint           not null, primary key
+#  name        :string
+#  description :text
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#
+FactoryBot.define do
+  factory :category do
+    sequence(:name) { |n| "Categoría #{n}" }
+    description { Faker::Lorem.sentence }
+  end
+end
